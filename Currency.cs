14350069f7bbad47b82cdfbe6ebb5d6d51b5378b
pvt0109 <40521352+pvt0109@users.noreply.github.com>
2022-07-24 19:08:32 +0530

    Add files via upload

---
+++ Currency.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace API_Currency_Converter.Models
+{
+    public class Currency
+    {
+        public static string[] GetCurrencyTags()
+        {
+
+            
+            return new string[] {"eur", "usd", "jpy", "bgn", "inr"};
+        }
+
+        public static float GetCurrencyRateInEuro(string currency)
+        {
+            if (currency.ToLower() == "")
+                throw new ArgumentException("Invalid Argument! currency parameter cannot be empty!");
+            if (currency.ToLower() == "eur")
+                throw new ArgumentException("Invalid Argument! Cannot get exchange rate from EURO to EURO");
+
+            try
+            {
+                
+                string rssUrl = string.Concat("http://www.ecb.int/rss/fxref-", currency.ToLower() + ".html");
+
+                
+                System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+                doc.Load(rssUrl);
+
+                
+                System.Xml.XmlNamespaceManager nsmgr = new System.Xml.XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("rdf", "http://purl.org/rss/1.0/");
+                nsmgr.AddNamespace("cb", "http://www.cbwiki.net/wiki/index.php/Specification_1.1");
+
+                
+                System.Xml.XmlNodeList nodeList = doc.SelectNodes("//rdf:item", nsmgr);
+
+                
+                foreach (System.Xml.XmlNode node in nodeList)
+                {
+                    
+                    CultureInfo ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+                    ci.NumberFormat.CurrencyDecimalSeparator = ".";
+
+                    try
+                    {
+                        
+                        float exchangeRate = float.Parse(
+                            node.SelectSingleNode("//cb:statistics//cb:exchangeRate//cb:value", nsmgr).InnerText,
+                            NumberStyles.Any,
+                            ci);
+
+                        return exchangeRate;
+                    }
+                    catch { }
+                }
+
+                
+                return 0;
+            }
+            catch
+            {
+               
+                return 0;
+            }
+        }
+
+       
+        public static float GetExchangeRate(string from, string to, float amount = 1)
+        {
+            
+            if (from == null || to == null)
+                return 0;
+
+            if (from.ToLower() == "eur" && to.ToLower() == "eur")
+                return amount;
+
+            try
+            {
+                
+                float toRate = GetCurrencyRateInEuro(to);
+                float fromRate = GetCurrencyRateInEuro(from);
+
+                
+                if (from.ToLower() == "eur")
+                {
+                    return (amount * toRate);
+                }
+                else if (to.ToLower() == "eur")
+                {
+                    return (amount / fromRate);
+                }
+                else
+                {
+                    
+                    return (amount * toRate) / fromRate;
+                }
+            }
+            catch { return 0; }
+        }
+    }
+}
+    
---
+++ Currency_ConverterController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API_Currency_Converter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Currency_Converter.Controllers
+{
+    public class Currency_ConverterController : ApiController
+    {
+        Currency Currency = new Currency();
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(string from, int amount, string to)
+        {
+            //double USDtoINR;
+            //double conversion;
+            //if (from == "USD" && to=="INR")
+            //{
+            //    double oneUSDtoINR = 79.85;
+                
+            //    conversion = amount * oneUSDtoINR;
+            //}
+            //else if (from == "EUR" && to == "USD")
+            //{
+            //    double oneEURtoUSD = 1.02;
+
+            //    conversion = amount * oneEURtoUSD;
+            //}
+            return "Success";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        public float convert(string fromCurrency,float amount,string toCurrency)
+        {
+            //string fromCurrency = "EUR";
+            //string toCurrency = "USD";
+            //int amount = 1;
+
+            
+            string[] availableCurrency = Currency.GetCurrencyTags();
+           
+            
+            float exchangeRate = Currency.GetExchangeRate(fromCurrency, toCurrency, amount);
+
+            return exchangeRate;
+        }
+    }
+}
